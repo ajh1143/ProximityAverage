88e90c05002a234c55263429723623b45d41bd02
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-04-30 15:59:12 -0700

    Update ProximityAverage.py
    1) Found error in findMean(), missing return statement.
    2) Removed boolean int check

--- ProximityAverage.py
+++ ProximityAverage.py
@@ -1,51 +1,50 @@
 import sys
 import numpy as np
 
+#Program that takes input of N integers on a single line, separated by single spaces.
+#The program should then print the number from the list that is closest to the average of the N integers in the list.
+#If there is more than one solution, output the one that occurs first in the input list.
+
 class ProximityAverage(object):
 
-"""
-Program that takes input of N integers on a single line, separated by single spaces. 
-The program should then print the number from the list that is closest to the average of the N integers in the list. 
-If there is more than one solution, output the one that occurs first in the input list.
-"""
-
+    # Input N values, ex: 1 2 3 4 5
     def inputValue(self):
-        #Input N values
         N = input()
-        if N.isnumeric():
-            return N
-        else:
-            raise ValueError("Please Enter a Valid Integer")
-            inputValue()
-                    
+        return N
+
+    # Split our N on space, to an array named digits
     def stripSplit(self, N):
-        #Strip and Split N into digits
+        # Strip and Split N into digits
         digits = N.strip().split(" ")
         return digits
-        
+
+    # Create a list mapping digits to ints, named castDigits
     def digitMapper(self, digs):
-        #Map digits to ints as list
-        castDigits = list(map(int, digits))
+        # Map digits to ints as list
+        castDigits = list(map(int, digs))
         return castDigits
-        
+
     def findMean(self, mappedDigits):
-        #Calculate Mean of list, NumPy for ease
+        # Calculate Mean of list, NumPy for ease
         meanVal = np.mean(mappedDigits)
-    
-    def findMinAbsDif(self, meanValue)
-        #Find smallest abs dif for each element in list compared to mean
-        smallestDif = np.abs(castDigits-meanVal).argmin()
+        return meanVal
+
+    def findMinAbsDif(self, mappedDigits, meanValue):
+        # Find smallest abs dif for each element in list compared to mean
+        smallestDif = np.abs(mappedDigits - meanValue).argmin()
         return smallestDif
-    
+
+
     def finalAnswer(self, mappedDigits, smallestDifference):
-        #Access index position of smallestDif in castDigits list of values
+        # Access index position of smallestDif in castDigits list of values
         print(mappedDigits[smallestDifference])
-        
+
+
 if __name__ == "__main__":
     a = ProximityAverage()
     num = a.inputValue()
     digits = a.stripSplit(num)
     mapperResult = a.digitMapper(digits)
     mean = a.findMean(mapperResult)
-    smallest = a.findMinAbsDif(mean)
+    smallest = a.findMinAbsDif(mapperResult, mean)
     a.finalAnswer(mapperResult, smallest)
